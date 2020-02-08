7832d540e60b9256617e90b32998b6ce7aede372
Sascha Zieger <saschazieger@gmail.com>
2020-02-08 19:22:10 +0100

    machine fixes

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -1,6 +1,6 @@
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "1.1.55"
-    const val versionNumber = 5
+    const val version = "1.1.56"
+    const val versionNumber = 6
 }
