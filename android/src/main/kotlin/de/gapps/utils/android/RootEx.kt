f69a5bd71c0da1e2bd8542c71b7491cb7df2747b
Sascha Zieger <saschazieger@gmail.com>
2020-02-25 19:58:06 +0100

    outcommented unsused code

--- android/src/main/kotlin/de/gapps/utils/android/RootEx.kt
+++ android/src/main/kotlin/de/gapps/utils/android/RootEx.kt
@@ -1,7 +1,5 @@
 package de.gapps.utils.android
 
-import de.gapps.utils.coroutines.runCommand
-
-suspend fun root(block: () -> Unit) {
-    "su".runCommand(block = block)
-}
+//suspend fun root(block: () -> Unit) {
+//    "su".runCommand(block = block)
+//}
--- android/src/main/kotlin/de/gapps/utils/android/WebMockTest.kt
+++ android/src/main/kotlin/de/gapps/utils/android/WebMockTest.kt
@@ -1,19 +1,11 @@
 package de.gapps.utils.android
 
-import android.app.Activity
-import androidx.test.filters.SmallTest
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnitRunner
-import de.gapps.utils.testing.IWebMock
-import org.junit.Rule
-import kotlin.reflect.KClass
-
-inline fun <reified A : Activity> webMockTest(webMock: IWebMock) = WebMockTest(webMock, A::class)
-
-@SmallTest
-open class WebMockTest<A : Activity>(
-    private val webMock: IWebMock,
-    clazz: KClass<A>,
-    @get:Rule
-    var rule: ActivityTestRule<A> = ActivityTestRule(clazz.java)
-) : AndroidJUnitRunner(), IWebMock by webMock
+//inline fun <reified A : Activity> webMockTest(webMock: IWebMock) = WebMockTest(webMock, A::class)
+//
+//@SmallTest
+//open class WebMockTest<A : Activity>(
+//    private val webMock: IWebMock,
+//    clazz: KClass<A>,
+//    @get:Rule
+//    var rule: ActivityTestRule<A> = ActivityTestRule(clazz.java)
+//) : AndroidJUnitRunner(), IWebMock by webMock
