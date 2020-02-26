b16aa77b9dc19bb9207341116bbfbfc0aa750566
Sascha Zieger <saschazieger@gmail.com>
2020-02-26 04:10:56 +0100

    1.1.91

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,7 +1,7 @@
 object Android {
 
     const val apiLevel = 29
-    const val buildTools = "29.0.2"
+    const val buildTools = "29.0.3"
     const val minSdk = 14
     const val targetSdk = 29
     const val versionCode = Globals.versionNumber
--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -1,6 +1,6 @@
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "1.1.89"
-    const val versionNumber = 26
+    const val version = "1.1.91"
+    const val versionNumber = 28
 }
