4acee1a87e9144bf78cc437225916d1297f5925f
Sascha Zieger <saschazieger@gmail.com>
2020-02-26 04:05:16 +0100

    1.1.89

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -1,6 +1,6 @@
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "1.1.88"
-    const val versionNumber = 25
+    const val version = "1.1.89"
+    const val versionNumber = 26
 }
