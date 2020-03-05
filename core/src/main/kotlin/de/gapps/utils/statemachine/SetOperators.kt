dd87b6984dc9c881e5785eb614537fe54f30a7ce
Sascha Zieger <saschazieger@gmail.com>
2020-03-05 17:57:08 +0100

    state machine data

--- core/src/main/kotlin/de/gapps/utils/statemachine/SetOperators.kt
+++ core/src/main/kotlin/de/gapps/utils/statemachine/SetOperators.kt
@@ -1,8 +1,10 @@
 package de.gapps.utils.statemachine
 
 
-operator fun IEvent.unaryPlus(): Set<IEvent> = setOf(this)
-operator fun IEvent.plus(other: IEvent): Set<IEvent> = setOf(this, other)
+operator fun <D : Any> IEvent<D>.unaryPlus(): Set<IEvent<D>> = setOf(this)
+operator fun <D : Any> IEvent<D>.times(other: IEvent<D>): Set<IEvent<D>> = setOf(this, other)
+operator fun <D : Any> Set<IEvent<D>>.times(other: IEvent<D>): Set<IEvent<D>> = setOf(*this.toTypedArray(), other)
 
-operator fun IState.unaryPlus(): Set<IState> = setOf(this)
-operator fun IState.plus(other: IState): Set<IState> = setOf(this, other)
+operator fun <D : Any> IState<D>.unaryPlus(): Set<IState<D>> = setOf(this)
+operator fun <D : Any> IState<D>.times(other: IState<D>): Set<IState<D>> = setOf(this, other)
+operator fun <D : Any> Set<IState<D>>.times(other: IState<D>): Set<IState<D>> = setOf(*this.toTypedArray(), other)
