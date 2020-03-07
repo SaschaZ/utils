7ef9b489a7e5d8fdeeecf2c389f0e11a19610c35
Sascha Zieger <saschazieger@gmail.com>
2020-03-07 21:54:25 +0100

    1.1.116

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,14 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.3.2.0")
     }
 }
 
 plugins {
     kotlin("jvm") version "1.3.70"
-    id("org.jetbrains.dokka") version "0.10.1"
+    id("org.jetbrains.dokka") version "0.9.17"
 }
 
 allprojects {
