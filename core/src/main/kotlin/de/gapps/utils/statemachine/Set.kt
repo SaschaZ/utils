e6c0ef0df1b87cf3967b34091e6782f70ad03710
Sascha Zieger <saschazieger@gmail.com>
2020-03-10 07:47:13 +0100

    1.1.123

--- core/src/main/kotlin/de/gapps/utils/statemachine/Set.kt
+++ core/src/main/kotlin/de/gapps/utils/statemachine/Set.kt
@@ -2,17 +2,22 @@
 
 package de.gapps.utils.statemachine
 
+import de.gapps.utils.statemachine.BaseType.Event
 import de.gapps.utils.statemachine.BaseType.ValueDataHolder
 
 val IMachineEx.set get() = SetScope(this)
 
 class SetScope(machine: IMachineEx) : IMachineEx by machine {
 
-    infix fun event(event: ValueDataHolder) {
-        this.event = event
+    infix fun event(event: BaseType) {
+        when (event) {
+            is Event -> this.event = ValueDataHolder(event)
+            is ValueDataHolder -> this.event = event
+            else -> throw IllegalArgumentException("Can not set type ${event::class} as an event.")
+        }
     }
 
-    suspend infix fun eventSync(event: ValueDataHolder) {
+    suspend infix fun eventSync(event: BaseType) {
         event(event)
         suspendUtilProcessingFinished()
     }
