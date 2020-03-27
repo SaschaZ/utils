413ec7c31398c5a4f8fa533a106aa160bdf98b15
Sascha Zieger <saschazieger@gmail.com>
2020-03-27 15:12:43 +0100

    1.1.139

--- buildSrc/src/main/kotlin/de/gapps/utils/Globals.kt
+++ buildSrc/src/main/kotlin/de/gapps/utils/Globals.kt
@@ -3,6 +3,6 @@ package de.gapps.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "1.1.138"
-    const val versionNumber = 62
+    const val version = "1.1.139"
+    const val versionNumber = 63
 }
