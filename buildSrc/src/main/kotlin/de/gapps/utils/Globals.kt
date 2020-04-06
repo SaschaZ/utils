95fd5887e899d547686619251f28a427c6e39b62
Sascha Zieger <saschazieger@gmail.com>
2020-04-06 23:15:58 +0200

    1.1.155

--- buildSrc/src/main/kotlin/de/gapps/utils/Globals.kt
+++ buildSrc/src/main/kotlin/de/gapps/utils/Globals.kt
@@ -3,6 +3,6 @@ package de.gapps.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "1.1.154"
-    const val versionNumber = 78
+    const val version = "1.1.155"
+    const val versionNumber = 79
 }
--- core/src/main/kotlin/de/gapps/utils/json/JsonConverter.kt
+++ core/src/main/kotlin/de/gapps/utils/json/JsonConverter.kt
@@ -2,23 +2,31 @@ package de.gapps.utils.json
 
 import de.gapps.utils.misc.catch
 import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.builtins.list
 import kotlinx.serialization.json.Json.Default.parse
 import kotlinx.serialization.json.Json.Default.stringify
 import kotlinx.serialization.serializer
 
-@ImplicitReflectionSerializer
 open class JsonConverter {
 
+    @UnstableDefault
+    @ImplicitReflectionSerializer
     inline fun <reified T : Any> T.toJson(): String? =
         catch(null) { stringify(T::class.serializer(), this) }
 
+    @UnstableDefault
+    @ImplicitReflectionSerializer
     inline fun <reified T : Any> List<T>.toJson(): String? =
         catch(null) { stringify(T::class.serializer().list, this) }
 
+    @UnstableDefault
+    @ImplicitReflectionSerializer
     inline fun <reified T : Any> String.fromJson(): T? =
         catch(null) { parse(T::class.serializer(), this) }
 
+    @UnstableDefault
+    @ImplicitReflectionSerializer
     inline fun <reified T : Any> String.fromJsonList(): List<T>? =
         catch(null) { parse(T::class.serializer().list, this) }
 }
