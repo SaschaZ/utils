f57c6bbd2009f74421671f522bbf23d62bda8015
Sascha Zieger <saschazieger@gmail.com>
2020-04-30 15:01:20 +0200

    1.2.7

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.ziger.utils"
-    const val version = "1.2.6"
-    const val versionNumber = 106
+    const val version = "1.2.7"
+    const val versionNumber = 107
 }
--- core/src/main/kotlin/dev/zieger/utils/time/TimeEx.kt
+++ core/src/main/kotlin/dev/zieger/utils/time/TimeEx.kt
@@ -4,54 +4,53 @@ import dev.zieger.utils.time.base.TimeUnit
 import dev.zieger.utils.time.base.TimeUnit.MS
 import dev.zieger.utils.time.base.toMillis
 import dev.zieger.utils.time.duration.IDurationHolder
-import kotlinx.serialization.*
 import java.util.*
 
-@Serializable
+//@Serializable
 open class TimeEx(
     override val millis: Long = System.currentTimeMillis(),
     override val nanos: Long = System.nanoTime(),
     override val zone: TimeZone = TimeZone.getDefault()
 ) : ITimeEx {
 
-    @Serializer(forClass = TimeEx::class)
-    companion object : TimeParseHelper(), KSerializer<TimeEx> {
-        @ImplicitReflectionSerializer
-        override val descriptor: SerialDescriptor = SerialDescriptor("TimeEx") {
-            element<String>("millis")
-            element<String>("nanos")
-            element<String>("zone")
-        }
-
-        @ImplicitReflectionSerializer
-        override fun deserialize(decoder: Decoder): TimeEx {
-            var millis: Long = -1L
-            var nanos: Long = -1L
-            lateinit var zone: TimeZone
-
-            decoder.beginStructure(descriptor).apply {
-                loop@ while (true) {
-                    when (val i = decodeElementIndex(descriptor)) {
-                        CompositeDecoder.READ_DONE -> break@loop
-                        0 -> millis = decodeLongElement(descriptor, i)
-                        1 -> nanos = decodeLongElement(descriptor, i)
-                        2 -> zone = TimeZone.getTimeZone(decodeStringElement(descriptor, i))
-                        else -> throw SerializationException("Unknown index $i")
-                    }
-                }
-            }
-            return TimeEx(millis, nanos, zone)
-        }
-
-        @ImplicitReflectionSerializer
-        override fun serialize(encoder: Encoder, value: TimeEx) {
-            encoder.beginStructure(descriptor).apply {
-                encodeLongElement(descriptor, 0, value.millis)
-                encodeLongElement(descriptor, 1, value.nanos)
-                encodeStringElement(descriptor, 2, value.zone.id)
-                endStructure(descriptor)
-            }
-        }
+    //    @Serializer(forClass = TimeEx::class)
+    companion object : TimeParseHelper() {//, KSerializer<TimeEx> {
+//        @ImplicitReflectionSerializer
+//        override val descriptor: SerialDescriptor = SerialDescriptor("TimeEx") {
+//            element<String>("millis")
+//            element<String>("nanos")
+//            element<String>("zone")
+//        }
+//
+//        @ImplicitReflectionSerializer
+//        override fun deserialize(decoder: Decoder): TimeEx {
+//            var millis: Long = -1L
+//            var nanos: Long = -1L
+//            lateinit var zone: TimeZone
+//
+//            decoder.beginStructure(descriptor).apply {
+//                loop@ while (true) {
+//                    when (val i = decodeElementIndex(descriptor)) {
+//                        CompositeDecoder.READ_DONE -> break@loop
+//                        0 -> millis = decodeLongElement(descriptor, i)
+//                        1 -> nanos = decodeLongElement(descriptor, i)
+//                        2 -> zone = TimeZone.getTimeZone(decodeStringElement(descriptor, i))
+//                        else -> throw SerializationException("Unknown index $i")
+//                    }
+//                }
+//            }
+//            return TimeEx(millis, nanos, zone)
+//        }
+//
+//        @ImplicitReflectionSerializer
+//        override fun serialize(encoder: Encoder, value: TimeEx) {
+//            encoder.beginStructure(descriptor).apply {
+//                encodeLongElement(descriptor, 0, value.millis)
+//                encodeLongElement(descriptor, 1, value.nanos)
+//                encodeStringElement(descriptor, 2, value.zone.id)
+//                endStructure(descriptor)
+//            }
+//        }
     }
 
     constructor(value: Number, timeUnit: TimeUnit = MS, timeZone: TimeZone = TimeZone.getDefault()) :
