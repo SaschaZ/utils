6a31c30383d210b63bab36dcf9436e9918ade2df
Sascha Zieger <saschazieger@gmail.com>
2020-05-10 08:08:11 +0200

    ParameterMix KDoc

--- core-testing/src/main/kotlin/dev/zieger/utils/core_testing/ParameterMix.kt
+++ core-testing/src/main/kotlin/dev/zieger/utils/core_testing/ParameterMix.kt
@@ -1,24 +1,41 @@
 package dev.zieger.utils.core_testing
 
+import dev.zieger.utils.misc.asUnit
 import dev.zieger.utils.misc.runEach
+import kotlinx.coroutines.channels.Channel
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-
-inline fun <reified T : Any> parameterMix(
-    contextBuilder: (Map<String, ParamInstance<*>>) -> T,
+/**
+ *
+ *
+ * @param inputFactory Is called to create the input parameter [I] out of a [Map] of parameter names with their values.
+ * Returned instance should be a class that uses [bind] to map the the input parameter to it's corresponding property in
+ * the input class [I].
+ * @param params Possible parameter names with a [List] of their possible values.
+ * @param block Is called for every generated input parameter [I]. Must return it's result as a [List] of [R].
+ *
+ * @return [Map] of input values [I] mapped to their results of type [R].
+ */
+inline fun <I, R> parameterMixCollect(
+    inputFactory: (Map<String, ParamInstance<*>>) -> I,
     vararg params: Pair<String, Param<*>>,
-    block: T.() -> Unit
-) = params.toList().mix().runEach { contextBuilder(this).block() }
+    block: I.() -> Channel<R>
+): Map<I, Channel<R>> = params.toList().mix().runEach { inputFactory(this).run { block().let { this to it } } }.toMap()
+
+inline fun <I> parameterMix(
+    inputFactory: (Map<String, ParamInstance<*>>) -> I,
+    vararg params: Pair<String, Param<*>>,
+    block: I.() -> Unit
+): Unit = parameterMixCollect(inputFactory, *params) { block(); Channel<Any>() }.asUnit()
 
 fun List<Pair<String, Param<*>>>.mix(): List<Map<String, ParamInstance<*>>> {
-    var expectedNumberCombinations = 1
-    forEach { expectedNumberCombinations *= it.second.list.size }
+    val expectedNumberCombinations: Int? = accumulate { accu, value -> (accu ?: 1) * value.second.list.size }
     println("number of combinations: $expectedNumberCombinations")
 
-    var result: List<List<ParamInstance<*>>> = emptyList()
-    forEach { result = result.mixWithParam(it) }
+    val result: List<List<ParamInstance<*>>> =
+        accumulate { accu, value -> (accu ?: ArrayList()).mixWithParam(value) } ?: emptyList()
     if (result.size != expectedNumberCombinations)
         throw IllegalStateException("Expected number of combinations does not match resulting number of combinations.")
     return result.map { it.map { i -> i.name to i }.toMap() }
@@ -77,6 +94,15 @@ inline fun <reified T : Any> param(name: String, list: List<T?>) =
 inline fun <reified T : Any> param(name: String, iterable: Iterable<T?>) =
     name to Param(T::class, iterable.toList())
 
+/**
+ * [ReadOnlyProperty] map the parameter to the corresponding property by their names.
+ */
 inline fun <reified T : Any?> bind(map: Map<String, ParamInstance<*>>) = object : ReadOnlyProperty<Any?, T> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): T = map.getValue(property.name).value as T
+}
+
+inline fun <A, T> Collection<T>.accumulate(block: (A?, T) -> A): A? {
+    var accu: A? = null
+    forEach { accu = block(accu, it) }
+    return accu
 }
