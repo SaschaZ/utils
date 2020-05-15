eb92a357ad53be0962af2b74aabcadad0f29ae87
Sascha Zieger <saschazieger@gmail.com>
2020-05-16 01:20:16 +0200

    made controllable extend onchanged

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "2.1.16"
-    const val versionNumber = 116
+    const val version = "2.1.18"
+    const val versionNumber = 118
 }
