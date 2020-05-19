ba910bae7c3cc5753b62b255630bdeef203f6bfc
Sascha Zieger <saschazieger@gmail.com>
2020-05-19 13:41:42 +0200

    duration arithmetic div result changed to Number

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "2.1.24"
-    const val versionNumber = 124
+    const val version = "2.1.25"
+    const val versionNumber = 125
 }
