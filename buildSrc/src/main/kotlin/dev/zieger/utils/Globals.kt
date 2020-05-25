c28cc69893863af0bf1e984cd28a014884afa69f
Sascha Zieger <saschazieger@gmail.com>
2020-05-25 21:38:21 +0200

    2.2.3

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "2.2.2"
-    const val versionNumber = 132
+    const val version = "2.2.3"
+    const val versionNumber = 133
 }
--- core/src/main/kotlin/dev/zieger/utils/log/LogCalls.kt
+++ core/src/main/kotlin/dev/zieger/utils/log/LogCalls.kt
@@ -6,20 +6,20 @@ import kotlinx.coroutines.CoroutineScope
  * Log-Calls
  */
 interface ILogCalls {
-    fun v(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun d(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun i(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun w(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun e(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun v(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun d(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun i(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun w(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun e(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
     fun e(throwable: Throwable, msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
 }
 
 interface ICoroutineLogCalls {
-    fun CoroutineScope.v(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun CoroutineScope.d(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun CoroutineScope.i(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun CoroutineScope.w(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
-    fun CoroutineScope.e(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun CoroutineScope.v(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun CoroutineScope.d(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun CoroutineScope.i(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun CoroutineScope.w(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
+    fun CoroutineScope.e(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null)
     fun CoroutineScope.e(
         throwable: Throwable,
         msg: String = "",
@@ -29,11 +29,11 @@ interface ICoroutineLogCalls {
 }
 
 interface IInlineLogCalls {
-    fun <T> T.logV(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
-    fun <T> T.logD(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
-    fun <T> T.logI(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
-    fun <T> T.logW(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
-    fun <T> T.logE(msg: String, vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
+    fun <T> T.logV(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
+    fun <T> T.logD(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
+    fun <T> T.logI(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
+    fun <T> T.logW(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
+    fun <T> T.logE(msg: String = "", vararg tag: String = emptyArray(), filter: ILogFilter? = null): T
     fun <T> T.logE(
         throwable: Throwable,
         msg: String = "$throwable",
