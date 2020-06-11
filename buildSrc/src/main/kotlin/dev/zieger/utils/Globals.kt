dc6bc57ac0db97fd7c92b798a7f8135c3144c94b
Sascha Zieger <saschazieger@gmail.com>
2020-06-11 09:59:08 +0200

    "2.2.12"

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "2.2.11"
-    const val versionNumber = 149
+    const val version = "2.2.12"
+    const val versionNumber = 150
 }
