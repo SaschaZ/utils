b44b2938993cf68742e32e380f00514f9cbe4170
Sascha Zieger <saschazieger@gmail.com>
2020-06-13 23:27:45 +0200

    set language kotlin in codacy

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 
 export CODACY_PROJECT_TOKEN=$1
 curl -LS -o codacy-coverage-reporter-assembly.jar "$(curl -LSs https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | endswith(".jar"))) | .[0].browser_download_url')"
-java -jar codacy-coverage-reporter-assembly.jar report -l Java -r ./core/build/jacoco/test/jacocoTestReport.xml
+java -jar codacy-coverage-reporter-assembly.jar report --language Kotlin --force-language -r ./core/build/jacoco/test/jacocoTestReport.xml
