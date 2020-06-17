cdc0cafa378a38488c6fd6ef1ca4f8324033fbd4
Sascha Zieger <saschazieger@gmail.com>
2020-06-18 01:58:03 +0200

    TypeContinuation

--- core/src/main/kotlin/dev/zieger/utils/coroutines/Continuation.kt
+++ core/src/main/kotlin/dev/zieger/utils/coroutines/Continuation.kt
@@ -8,6 +8,58 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.channels.Channel
 
+/**
+ * Allows to suspend until a method is called.
+ */
+interface ITypeContinuation<T> {
+
+    /**
+     * Will suspend the current coroutine until [trigger] or [triggerS] gets called.
+     *
+     * @param timeout When suspending longer than defined in [timeout] a [TimeoutCancellationException] is thrown.
+     * If `null` no timeout is used. Defaulting to `null`.
+     */
+    suspend fun suspendUntilTrigger(
+        wanted: T? = null,
+        timeout: IDurationEx? = null
+    ): T?
+
+    /**
+     * Triggers the continuation. Suspends if necessary.
+     *
+     * @param timeout When suspending longer than defined in [timeout] a [TimeoutCancellationException] is thrown.
+     * If `null` no timeout is used. Defaulting to `null`.
+     */
+    suspend fun triggerS(value: T, timeout: IDurationEx? = null)
+
+    /**
+     * Triggers the continuation. Will launch a new coroutine if the underlying channel can not receive new values.
+     *
+     * @param timeout When the new launched coroutine is suspending longer than defined in [timeout] a
+     * [TimeoutCancellationException] is thrown. If `null` no timeout is used. Defaulting to `null`.
+     */
+    fun trigger(value: T, timeout: IDurationEx? = null)
+}
+
+open class TypeContinuation<T>(
+    private val channel: Channel<T> = Channel(),
+    private val scope: CoroutineScope = DefaultCoroutineScope()
+) : ITypeContinuation<T> {
+
+    override suspend fun suspendUntilTrigger(
+        wanted: T?,
+        timeout: IDurationEx?
+    ): T? = withTimeout(timeout) {
+        var result: T = channel.receive()
+        while (wanted?.let { result != it } == true) result = channel.receive()
+        result
+    }
+
+    override suspend fun triggerS(value: T, timeout: IDurationEx?) = withTimeout(timeout) { channel.send(value) }
+
+    override fun trigger(value: T, timeout: IDurationEx?) =
+        if (!channel.offer(value)) scope.launchEx { triggerS(value, timeout) }.asUnit() else Unit
+}
 
 /**
  * Allows to suspend until a method is called.
@@ -20,8 +72,7 @@ interface IContinuation {
      * @param timeout When suspending longer than defined in [timeout] a [TimeoutCancellationException] is thrown.
      * If `null` no timeout is used. Defaulting to `null`.
      */
-    suspend fun suspendUntilTrigger(timeout: IDurationEx? = null) {
-    }
+    suspend fun suspendUntilTrigger(timeout: IDurationEx? = null)
 
     /**
      * Triggers the continuation. Suspends if necessary.
