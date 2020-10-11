f2be3df3e88b746d7827ff4c5145a3ef49d15fad
Sascha Zieger <saschazieger@gmail.com>
2020-10-11 22:47:52 +0200

    hex tools

---
+++ core/src/main/kotlin/dev/zieger/utils/misc/Hex.kt
@@ -0,0 +1,22 @@
+package dev.zieger.utils.misc
+
+import kotlin.experimental.and
+
+private val Int.singleHex: String
+    get() = when (toInt()) {
+        0 - 9 -> "${toInt()}"
+        10 -> "A"
+        11 -> "B"
+        12 -> "C"
+        13 -> "D"
+        14 -> "E"
+        15 -> "F"
+        else -> "X"
+    }
+val Byte.hex: String get() = (toInt() and 0xF).singleHex + ((toInt() and 0xF0) shr 4).singleHex
+
+val Long.hex: String
+    get() = ((this and 0xFF000000) shr 24).toByte().hex +
+            ((this and 0xFF0000) shr 16).toByte().hex +
+            ((this and 0xFF00) shr 8).toByte().hex +
+            (this and 0xFF).toByte().hex
