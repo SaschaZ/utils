bfbbe701b27ec3053de1114a6cdb06f6ecc10baa
Sascha Zieger <saschazieger@gmail.com>
2021-01-16 16:23:00 +0100

    test coroutine scope

--- core-testing/src/main/kotlin/dev/zieger/utils/core_testing/TestCoroutineScope.kt
+++ core-testing/src/main/kotlin/dev/zieger/utils/core_testing/TestCoroutineScope.kt
@@ -2,7 +2,10 @@
 
 package dev.zieger.utils.core_testing
 
+import dev.zieger.utils.coroutines.scope.DefaultCoroutineScope
 import dev.zieger.utils.coroutines.scope.ICoroutineScopeEx
+import dev.zieger.utils.coroutines.scope.IoCoroutineScope
+import dev.zieger.utils.coroutines.scope.UnconfinedCoroutineScope
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
@@ -24,4 +27,20 @@ class TestCoroutineScope : ICoroutineScopeEx {
         get() = Job() + object : CoroutineDispatcher() {
             override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
         }
+}
+
+class TestCoroutineDispatcher : CoroutineDispatcher() {
+    override fun dispatch(context: CoroutineContext, block: Runnable) = block.run()
+}
+
+open class TestDefaultCoroutineScope : DefaultCoroutineScope() {
+    override val coroutineContext: CoroutineContext = TestCoroutineDispatcher() + Job()
+}
+
+open class TestIoCoroutineScope : IoCoroutineScope() {
+    override val coroutineContext: CoroutineContext = TestCoroutineDispatcher() + Job()
+}
+
+open class TestUnconfinedCoroutineScope : UnconfinedCoroutineScope() {
+    override val coroutineContext: CoroutineContext = TestCoroutineDispatcher() + Job()
 }
--- jdk/src/main/kotlin/dev/zieger/utils/gui/console/SystemInfo.kt
+++ jdk/src/main/kotlin/dev/zieger/utils/gui/console/SystemInfo.kt
@@ -1,11 +1,7 @@
 package dev.zieger.utils.gui.console
 
-import dev.zieger.utils.OsInfo
-import dev.zieger.utils.OsInfo.OsType.LINUX
-import dev.zieger.utils.OsInfo.OsType.MACOS
 import dev.zieger.utils.coroutines.builder.launchEx
 import dev.zieger.utils.coroutines.exec
-import dev.zieger.utils.coroutines.runCommand
 import dev.zieger.utils.gui.console.ConsoleProgressBar.Companion.PROGRESS_COLORS
 import dev.zieger.utils.gui.console.LanternaConsole.Companion.refresh
 import dev.zieger.utils.gui.console.ProcessInfo.Companion.DEFAULT_AUTO_UPDATE_INTERVAL
@@ -13,7 +9,6 @@ import dev.zieger.utils.gui.console.ProgressEntity.*
 import dev.zieger.utils.gui.console.ProgressUnit.Bytes
 import dev.zieger.utils.misc.asUnit
 import dev.zieger.utils.misc.nullWhen
-import dev.zieger.utils.misc.whenNotNull
 import dev.zieger.utils.time.delay
 import dev.zieger.utils.time.duration.IDurationEx
 import dev.zieger.utils.time.duration.seconds
@@ -52,70 +47,70 @@ class TopInfo(
 
     private val pid: Long get() = ManagementFactory.getRuntimeMXBean().name.split("@").first().toLong()
 
-    init {
-        autoUpdateInterval?.also {
-            autoUpdateJob = scope.launchEx(interval = it) { update() }
-        }
-    }
+//    init {
+//        autoUpdateInterval?.also {
+//            autoUpdateJob = scope.launchEx(interval = it) { update() }
+//        }
+//    }
 
-    suspend fun update() {
-        cachedOutput = null
-        cpuPercent = determineCpuPercent()
-        memPercent = determineMemPercent()
-        onUpdate()
-    }
-
-    private suspend fun determineCpuPercent(): Double = when (OsInfo.type) {
-        MACOS -> "CPU usage: ([0-9.]+)% user, ([0-9.]+)% sys, ([0-9.]+)% idle".toRegex()
-            .find(cachedOutput ?: "top -l 1 -n 1".runCommand().stdOutput.also { cachedOutput = it })?.groupValues?.run {
-                whenNotNull(getOrNull(1)?.toDoubleOrNull(), getOrNull(2)?.toDoubleOrNull()) { user, sys ->
-                    (user + sys) / 100
-                }
-            }
-        LINUX -> "%Cpu\\(s\\): {2}([0-9,.]+) us, {2}([0-9,.]+) sy, {2}([0-9,.]+) ni, ([0-9,.]+) id".toRegex()
-            .find("top -n 1 -1".runCommand().stdOutput)?.groupValues?.run {
-                whenNotNull(getOrNull(1)?.toDoubleOrNull(), getOrNull(2)?.toDoubleOrNull()) { user, sys ->
-                    (user + sys) / 100
-                }
-            }
-        else -> null
-    } ?: -1.0
-
-    private suspend fun updateAll() {
-        when (OsInfo.type) {
-            MACOS -> ("""[\w]+: ([\d]+)([KMG]?) [\w]+ \(([\d]+)([KMG]?) [\w]+\), ([\d]+)([KMG]?) [\w]+.[\W\w]+[\w]+ +([\d.]+) +([\d]+)([KMG]?)([-+]?)""").toRegex()
-                .find(
-                    cachedOutput ?: "top -l 2 -stats command,cpu,mem -pid $pid".runCommand().stdOutput.also {
-                        cachedOutput = it
-                    })?.groupValues?.run {
-                    whenNotNull(
-                        getOrNull(5)?.toDoubleOrNull(), getOrNull(6),
-                        getOrNull(7)?.toDoubleOrNull(), getOrNull(8)?.toIntOrNull(), getOrNull(9)
-                    ) { unused, unusedUnit, cpu, mem, memUnit ->
-                        totalMem = "sysctl hw.memsize".runCommand().stdOutput.split(" ")[1].toDouble()
-                        availMem = unused * unusedUnit.unitFactor
-                        overallUsedMem = totalMem - availMem
-                        usedMem = mem * memUnit.unitFactor.toDouble()
-                        memPercent = overallUsedMem / totalMem
-                        cpuPercent = cpu
-                    }
-                }
-            LINUX -> ("Mem: *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*)").toRegex()
-                .find("free -m | grep Mem:".runCommand().stdOutput)?.groupValues?.run {
-                    whenNotNull(
-                        getOrNull(1)?.toDoubleOrNull(),
-                        getOrNull(2)?.toDoubleOrNull(),
-                        getOrNull(3)?.toDoubleOrNull()
-                    ) { total, used, avail ->
-                        totalMem = total * 1024 * 1024
-                        usedMem = used * 1024 * 1024
-                        availMem = avail * 1024 * 1024
-                        memPercent = usedMem / totalMem
-                    }
-                }
-            else -> Unit
-        }
-    }
+//    suspend fun update() {
+//        cachedOutput = null
+//        cpuPercent = determineCpuPercent()
+//        memPercent = determineMemPercent()
+//        onUpdate()
+//    }
+//
+//    private suspend fun determineCpuPercent(): Double = when (OsInfo.type) {
+//        MACOS -> "CPU usage: ([0-9.]+)% user, ([0-9.]+)% sys, ([0-9.]+)% idle".toRegex()
+//            .find(cachedOutput ?: "top -l 1 -n 1".runCommand().stdOutput.also { cachedOutput = it })?.groupValues?.run {
+//                whenNotNull(getOrNull(1)?.toDoubleOrNull(), getOrNull(2)?.toDoubleOrNull()) { user, sys ->
+//                    (user + sys) / 100
+//                }
+//            }
+//        LINUX -> "%Cpu\\(s\\): {2}([0-9,.]+) us, {2}([0-9,.]+) sy, {2}([0-9,.]+) ni, ([0-9,.]+) id".toRegex()
+//            .find("top -n 1 -1".runCommand().stdOutput)?.groupValues?.run {
+//                whenNotNull(getOrNull(1)?.toDoubleOrNull(), getOrNull(2)?.toDoubleOrNull()) { user, sys ->
+//                    (user + sys) / 100
+//                }
+//            }
+//        else -> null
+//    } ?: -1.0
+//
+//    private suspend fun updateAll() {
+//        when (OsInfo.type) {
+//            MACOS -> ("""[\w]+: ([\d]+)([KMG]?) [\w]+ \(([\d]+)([KMG]?) [\w]+\), ([\d]+)([KMG]?) [\w]+.[\W\w]+[\w]+ +([\d.]+) +([\d]+)([KMG]?)([-+]?)""").toRegex()
+//                .find(
+//                    cachedOutput ?: "top -l 2 -stats command,cpu,mem -pid $pid".runCommand().stdOutput.also {
+//                        cachedOutput = it
+//                    })?.groupValues?.run {
+//                    whenNotNull(
+//                        getOrNull(5)?.toDoubleOrNull(), getOrNull(6),
+//                        getOrNull(7)?.toDoubleOrNull(), getOrNull(8)?.toIntOrNull(), getOrNull(9)
+//                    ) { unused, unusedUnit, cpu, mem, memUnit ->
+//                        totalMem = "sysctl hw.memsize".runCommand().stdOutput.split(" ")[1].toDouble()
+//                        availMem = unused * unusedUnit.unitFactor
+//                        overallUsedMem = totalMem - availMem
+//                        usedMem = mem * memUnit.unitFactor.toDouble()
+//                        memPercent = overallUsedMem / totalMem
+//                        cpuPercent = cpu
+//                    }
+//                }
+//            LINUX -> ("Mem: *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*) *([0-9]*)").toRegex()
+//                .find("free -m | grep Mem:".runCommand().stdOutput)?.groupValues?.run {
+//                    whenNotNull(
+//                        getOrNull(1)?.toDoubleOrNull(),
+//                        getOrNull(2)?.toDoubleOrNull(),
+//                        getOrNull(3)?.toDoubleOrNull()
+//                    ) { total, used, avail ->
+//                        totalMem = total * 1024 * 1024
+//                        usedMem = used * 1024 * 1024
+//                        availMem = avail * 1024 * 1024
+//                        memPercent = usedMem / totalMem
+//                    }
+//                }
+//            else -> Unit
+//        }
+//    }
 
     private val String.unitFactor
         get() = when (this) {
