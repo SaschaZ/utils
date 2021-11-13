eb2afadfee4cb3db14005e87ff38ae85e3257c1e
Sascha Zieger <sascha@zieger.dev>
2021-11-13 08:45:38 +0100

    cast

--- misc/src/main/kotlin/dev/zieger/utils/misc/Cast.kt
+++ misc/src/main/kotlin/dev/zieger/utils/misc/Cast.kt
@@ -1,5 +1,7 @@
 package dev.zieger.utils.misc
 
-inline fun <reified T: Any> Any.cast(): T = this as T
+inline fun <reified T : Any> Any.cast(): T = this as T
+
+inline fun <reified T : Any> Any.castSafe(): T? = this as? T
 
 fun Any.asUnit(): Unit = Unit
