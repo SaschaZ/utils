90435c550e44933973367ea12aebd8ed9a16cf97
Sascha Zieger <zieger@karlmax-berlin.com>
2022-04-25 01:11:20 +0200

    3.0.14

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "3.0.13"
-    const val versionNumber = 21
+    const val version = "3.0.14"
+    const val versionNumber = 22
 }
