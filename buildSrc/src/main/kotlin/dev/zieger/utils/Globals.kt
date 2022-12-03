632d9797178f0b0a1be69c0e35599c18e2d4cae4
Sascha Zieger <saschazieger@gmail.com>
2022-12-03 21:08:35 +0100

    v3.0.22

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "3.0.17"
+    const val version = "3.0.22"
     const val versionNumber = 25
 }
