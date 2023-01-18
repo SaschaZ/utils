e9dd130f29e3898fcfe7176a427876590f9895bd
Sascha Zieger <saschazieger@gmail.com>
2023-01-18 06:18:59 +0100

    reverted buildSrc remove

--- buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
+++ buildSrc/src/main/kotlin/dev/zieger/utils/Globals.kt
@@ -3,6 +3,6 @@ package dev.zieger.utils
 object Globals {
 
     const val group = "dev.zieger.utils"
-    const val version = "3.0.23"
+    const val version = "3.0.24"
     const val versionNumber = 26
 }
